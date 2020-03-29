2933c551cc82101601a4bdeda1c6440fc2d9d7a3
Italo Abreu <italodw@gmail.com>
2020-03-29 16:52:35 -0300

    A área administrativa dos projetos Django e o arquivo admin.py

--- basico/django1/core/views.py
+++ basico/django1/core/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'curso': 'Prpgramação WEB com Django Framework',
+        'curso': 'Programação WEB com Django Framework',
         'outro': 'Django é massa!'
     }
     return render(request, 'index.html', context)
