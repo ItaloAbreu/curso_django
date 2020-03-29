97d6152d1f1a7a127e05374adcf56cf39f23cbed
Italo Abreu <italodw@gmail.com>
2020-03-29 17:26:46 -0300

    Usando e abusando do Django Shell

--- basico/django1/core/views.py
+++ basico/django1/core/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    print(dir(request.user))
+    print(f'User: {request.user}')
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuario Não Logado'
+    else:
+        teste = 'Usuario Logado'
+
     context = {
         'curso': 'Programação WEB com Django Framework',
-        'outro': 'Django é massa!'
+        'outro': 'Django é massa!',
+        'logado': teste
     }
     return render(request, 'index.html', context)
 
